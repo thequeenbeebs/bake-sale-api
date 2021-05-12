b9690d30f93691676dae21af2f70a94ee6fa3204
Blaire Baker <bakerblaire@gmail.com>
2021-05-12 12:25:01 -0600

    create db, build schema

--- db/migrate/20210511231521_user.rb
+++ db/migrate/20210511231521_user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Migration[6.0]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
   end
 end
--- db/migrate/20210511231534_item.rb
+++ db/migrate/20210511231534_item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Migration[6.0]
   def change
+    create_table :items do |t|
+      t.string :name
+      t.string :category
+      t.text :description
+      t.integer :price
+      t.integer :user_id
+    end
   end
 end
