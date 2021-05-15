8039185fae8833d06987b930ddcc80614d88b89b
Blaire Baker <bakerblaire@gmail.com>
2021-05-15 14:07:48 -0600

    add column for img

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Item.destroy_all
+
+blaire = User.create(username: "blaire", password: "blaire")
+drew = User.create(username: "drew", password: "drew")
+nichole = User.create(username: "nichole", password: "nichole")
+zoe = User.create(username: "zoe", password: "zoe")
+ryan = User.create(username: "ryan", passowrd: "ryan")
+
+Item.create()
